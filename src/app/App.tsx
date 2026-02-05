1cbed2264a0a8f09a45eb348d247d3a8bb6a1cad
dongjoon.lee <dongjoon.lee@jbbank.co.kr>
2026-02-05 21:34:02 +0900

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainApp } from './components/MainApp';
-import { MapPage } from './components/pages/MapPage';
-import { SettingsPage } from './components/pages/SettingsPage';
-import { ChargePage } from './components/pages/ChargePage';
-import { NotificationsPage } from './components/pages/NotificationsPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainApp } from "./components/MainApp";
+import { MapPage } from "./components/pages/MapPage";
+import { SettingsPage } from "./components/pages/SettingsPage";
+import { ChargePage } from "./components/pages/ChargePage";
+import { NotificationsPage } from "./components/pages/NotificationsPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/BlackSpoonDeveloper">
       <Routes>
         <Route path="/" element={<MainApp />} />
         <Route path="/map" element={<MapPage />} />
