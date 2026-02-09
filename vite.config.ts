95b2c1876659aeadb8106a787d0a48d94eeb5f4d
박진홍 <jinhongstar@gmail.com>
2026-02-09 13:49:59 +0900

    오류수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // GitHub Pages를 위한 상대 경로 설정
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
